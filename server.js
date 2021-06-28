1a3bacc26ba21409628f089bb82cee79c60ae5c2
Justin <justinreeves52@gmail.com>
2021-06-28 03:15:16 -0400

    server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const fetch = require("node-fetch");
 const path = require('path');
-const port = process.env.PORT || 3000;
+const port = 3000;
 const cors = require("cors");
 const app = express();
 app.use(logger);
