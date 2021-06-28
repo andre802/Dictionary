aa3814cdf9c4740817ab9a6281eec06c1ea46c45
Justin <justinreeves52@gmail.com>
2021-06-27 23:27:56 -0400

    build

--- server.js
+++ server.js
@@ -98,7 +98,7 @@ app.get("/search", async (req, res) => {
     let data = await getInfo(word)
     res.json(data);
 })
-app.get("*", (req,res) => {
+app.use((req,res) => {
     res.sendFile(path.resolve(__dirname, '/client/build','index.html'))
 })
 function logger(req, res, next) {
